0ad95810cf45199db1d377c498039e7a94940968
Damian Mayorano <162374514+dmayorano@users.noreply.github.com>
2024-11-20 22:05:27 -0300

    Delete HTLMCSSJS directory

--- HTLMCSSJS/script.js
+++
@@ -1,9 +0,0 @@
-const botonesCompartir = document.querySelectorAll('.compartir');
-
-botonesCompartir.forEach((boton) => {
-  boton.addEventListener('click', () => {
-    const enlace = window.location.href;
-    navigator.clipboard.writeText(enlace);
-    alert('Enlace copiado al portapapeles');
-  });
-});
